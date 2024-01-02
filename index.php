aa9528871d945dc1ae449a645c16e583a7273136
mdraihanhossen100 <155410828+mdraihanhossen100@users.noreply.github.com>
2024-01-02 10:50:14 -0800

    test
    testing

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+
+echo " Hello Developer Raihan ";
+echo " <br> ";
+echo " Hello Raihan ";
+
+/* comments hocca kon content ta hide korar jonnow use kora <hoy></hoy>*/
+
+
+$first_amout = 200;
+$last_amount = 400;
+
+echo " your first amount is $first_amout, your last amount is $last_amount ";
+/* variable another example*/
+
+echo " <br> ";
+
+$first_name = " raihan ";
+$last_name = " hossen ";
+echo " your total name $first_name your lat name $last_name  ";
+
+
+
+/*php comment*/
+
+$first_type = " Hello Bangladesh";
+
+
+/* php constant*/
+
+echo " <br> ";
+
+$first_name = "  Hello Raihan ";
+$second_name = " Hossen ";
+
+/* Remember me constant use korta hola mona rakta hoba constant a define nama function use korta hoy*/
+
+
+
+/* how to uses printf*/
+
+printf ( "hello raihan" );
+
+/* php ta html code show*/
+
+echo " <h1> Hello Developer Raihan  </h1> ";
+echo " <h4> this a developer team </h4> ";
+echo " <p> w are developer teams </p>";
+
+
+/* php ta html khub easy ta show korano jay*/
+
+
+
+
+/*php data type*/
+
+
+/* integer type data*/
+
+$first_number = 2000;
+$first_names = " md raihan patowary ";
+$first_float =  200.00 ;
+
+echo " $first_number ";
+echo " $first_names ";
+echo " $first_float ";
+
+
+/* bulian data type*/
+$bulian = " true ";
+$user_admin = " true ";
+$user_editor = " false ";
+
+echo " <br/> ";
+
+echo " $user_admin ";
+echo " <br/> ";
+
+echo " $user_editor ";
+
+
+/* how to php variable declare */
+
+$number = 200;
+$name = " Hello Raihan ";
+
+$decemel_number =  200.000;
+
+echo "<br/>";
+var_dump ( $number );
+
+echo "<br/>";
+
+var_dump ( $name );
+
+echo "<br/>";
+
+var_dump ( $decemel_number );
+
+echo "<br/>";
+
+/* how to uses php ucwords function*/
+
+$first_namess = " raihan hossen patowary vai ";
+
+$first_namesss = ucwords ( $first_namess );
+echo " $first_namesss ";
+
+?>
+
+<h1>   </h1>
+    
+</body>
+</html>
